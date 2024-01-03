cbc5fe8da45ecf74e195a6a251b305f17bf5f05f
mehran banbersta <mehranbanbersta@mehrans-MacBook-Pro.local>
2024-01-03 13:10:33 +0100

    add hr active in nav menu when user click on

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 const Navbar = () => {
+  const [menu, setMenu] = useState('shop');
+
   return (
     <div className="navbar">
+      {/* navbar-logo */}
       <div className="navbar-logo">
         <img src={logo} alt="" />
         <p className="navbar-logo-name">ABOUT-YOU</p>
       </div>
+      {/* navbar-menu */}
       <ul className="navbar-menu">
-        <li className="navbar-menu-item">Shop</li>
-        <li className="navbar-menu-item">Men</li>
-        <li className="navbar-menu-item">Women</li>
-        <li className="navbar-menu-item">Kids</li>
+        <li
+          onClick={() => {
+            setMenu('shop');
+          }}
+          className="navbar-menu-item"
+        >
+          Shop{menu === 'shop' ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu('mens');
+          }}
+          className="navbar-menu-item"
+        >
+          Men {menu === 'mens' ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu('womens');
+          }}
+          className="navbar-menu-item"
+        >
+          Women {menu === 'womens' ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu('kids');
+          }}
+          className="navbar-menu-item"
+        >
+          Kids {menu === 'kids' ? <hr /> : <></>}
+        </li>
       </ul>
+      {/* navbar-login-cart */}
       <div className="navbar-login-cart">
         <button className="navbar-login-button">Login</button>
         <img className="navbar-cart-img" src={cart_icon} alt="" />
