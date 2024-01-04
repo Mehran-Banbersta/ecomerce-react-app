9a2b7a81b741ae84d8375c3d72ae6a095ea99b1c
mehran banbersta <mehranbanbersta@mehrans-MBP.home>
2024-01-04 19:54:22 +0100

    imported banners for shope category

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Shope from './Pages/Shope';
+import Shope from './Pages/Home';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
 
 function App() {
   return (
@@ -14,13 +17,22 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shope />} />
-          <Route path="/mens" element={<ShopCategory category="mens" />} />
-          <Route path="/womens" element={<ShopCategory category="womens" />} />
-          <Route path="/kids" element={<ShopCategory category="kids" />} />
-          <Route path="/product" element={<Product />}/>
-          <Route path=":productId" element={<Product />}/>
-          <Route path="/cart" element={<Cart />}/>
-          <Route path="/login" element={<LoginSignup />}/>
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} category="kid" />}
+          />
+          <Route path="/product" element={<Product />} />
+          <Route path=":productId" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
       </BrowserRouter>
     </div>
