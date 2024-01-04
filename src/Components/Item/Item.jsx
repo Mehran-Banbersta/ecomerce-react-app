392fb779e548613dc40347f75422953a618eacea
mehran banbersta <mehranbanbersta@mehrans-MBP.home>
2024-01-04 20:01:01 +0100

    changed className of elements

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,9 +6,9 @@ const Item = (props) => {
     <div className="item-container">
       <img src={props.image} alt="" />
       <p>{props.name}</p>
-      <div className="item price">
-        <div className="item-price-new">{props.new_price}</div>
-        <div className="item-price-old">{props.old_price}</div>
+      <div className="item-container-price">
+        <div className="item-container-price-new">${props.new_price}</div>
+        <div className="item-container-price-old">${props.old_price}</div>
       </div>
     </div>
   );
