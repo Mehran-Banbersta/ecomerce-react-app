26f63b1be44611134d10900d30bdc46abb11e61b
mehran banbersta <mehranbanbersta@mehrans-MBP.home>
2024-01-15 12:51:52 +0100

    modify app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Shope from './Pages/Home';
-import ShopCategory from './Pages/ShopCategory';
-import Product from './Pages/Product';
-import Cart from './Pages/Cart';
-import LoginSignup from './Pages/LoginSignup';
+import ShopCategory from './Pages/ShopCategoryPage/ShopCategory.jsx';
+import Product from './Pages/ProductDetails/Product';
+import Cart from './Pages/Cart/Cart';
+import LoginSignup from './Pages/LoginSignupPage/LoginSignup';
 import men_banner from './Components/Assets/banner_mens.png';
 import women_banner from './Components/Assets/banner_women.png';
 import kid_banner from './Components/Assets/banner_kids.png';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -30,10 +31,11 @@ function App() {
             element={<ShopCategory banner={kid_banner} category="kid" />}
           />
           <Route path="/product" element={<Product />} />
-          <Route path=":productId" element={<Product />} />
+          <Route path="/product/:productId" element={<Product />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
