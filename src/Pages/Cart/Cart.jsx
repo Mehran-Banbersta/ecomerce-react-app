409c381d151435b07792755cb373c2d0a38bd506
mehran banbersta <mehranbanbersta@mehrans-MBP.home>
2024-01-15 12:59:12 +0100

    created the css file

---
+++ src/Pages/Cart/Cart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CartItems from '../../Components/CartItems/CartItems.jsx';
+import './Cart.css';
+
+const Cart = () => {
+  return (
+    <div className="cart-container">
+      
+      <CartItems />
+    </div>
+  );
+};
+
+export default Cart;
